617aa68f4156d41e2e4c4ecaa09dc4682eff0a52
Miraghaa <miraghaea@code.edu.az>
2023-09-07 09:09:30 +0400

    BE-002 Created category configuration

---
+++ src/Infrastucture/Fiorello.Persistence/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Fiorello.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Fiorello.Persistence.Configurations;
+
+public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+{
+	public void Configure(EntityTypeBuilder<Category> builder)
+	{
+		builder.Property(c=>c.Name).IsRequired().HasMaxLength(50);
+		builder.Property(c=>c.Description).HasMaxLength(250);
+	}
+}
