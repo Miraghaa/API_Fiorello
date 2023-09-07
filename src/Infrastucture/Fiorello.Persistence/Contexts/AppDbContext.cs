ad848d387c36a6e8459b711a1b145043e776797e
Miraghaa <miraghaea@code.edu.az>
2023-09-07 09:32:09 +0400

    BE-003 Connection Databse

---
+++ src/Infrastucture/Fiorello.Persistence/Contexts/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Fiorello.Domain.Entities;
+using Fiorello.Persistence.Configurations;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fiorello.Persistence.Contexts;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+    {
+    }
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
+	{
+		modelBuilder.ApplyConfigurationsFromAssembly(typeof(CategoryConfiguration).Assembly);
+		base.OnModelCreating(modelBuilder);
+	}
+	public DbSet<Category> Categories { get; set; } = null!;
+}
--- src/Presentation/Fierollo.API/Program.cs
+++ src/Presentation/Fierollo.API/Program.cs
@@ -1,7 +1,13 @@
+using Fiorello.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+	options.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
+});
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
